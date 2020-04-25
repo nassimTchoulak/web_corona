d00765f2e050546d683dff7b5a8e7030347810e5
tchoulak nassim <gn-tchoulak@esi.dz>
2020-04-25 11:27:50 +0100

    working2

---
+++ spec/support/spec.js
@@ -0,0 +1,20 @@
+import Login from "../../src/agent/Login";
+import React from "react";
+import {shallow} from 'enzyme';
+
+
+describe('Agent componenets',()=>{
+
+
+    const log = shallow(<Login/>)
+
+        log = shallow(<Login />);
+
+    it('image _loaded',()=>{
+        expect(log.find(""))
+
+    })
+
+
+
+})
